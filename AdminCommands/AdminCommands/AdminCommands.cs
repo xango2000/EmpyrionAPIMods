8b1158de3a79fa42289df73f7ddd62ce819d8fa6
Nicholas Nestor <xango2000@gmail.com>
2017-12-14 21:30:46 -0600

    Started work on Admin Commands

---
+++ AdminCommands/AdminCommands/AdminCommands.cs
@@ -0,0 +1,306 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Eleon.Modding;
+using ProtoBuf;
+using Tracker;
+
+namespace AdminCommands
+{
+    public class AdminCommands : ModInterface
+    {
+        ModGameAPI GameAPI;
+        public Dictionary<string, Players> PlayerDictionary = new Dictionary<string, Players> { };
+        ushort SeqNrCounter = 1500;
+        Dictionary<ushort, Tracker.Request.TrackData> SeqNrDict = new Dictionary<ushort, Request.TrackData> { };
+
+        private void Alert(int Priority, int player, String msg, float Duration)
+        {
+            //Priority 0 = Red
+            //Priority 1 = Yellow
+            //Priority 2 = Blue
+            GameAPI.Game_Request(CmdId.Request_InGameMessage_AllPlayers, (ushort)CmdId.Request_InGameMessage_AllPlayers, new IdMsgPrio(player, msg, Convert.ToByte(Priority), Duration));
+        }
+
+        private void ServerSay(int player, String msg)
+        {
+            if (player == 0)
+            {
+                string command = "SAY '" + msg + "'";
+                GameAPI.Game_Request(CmdId.Request_ConsoleCommand, (ushort)CmdId.Request_ConsoleCommand, new Eleon.Modding.PString(command));
+            }
+            else
+            {
+                string command = "SAY p:" + player + " '" + msg + "'";
+                GameAPI.Game_Request(CmdId.Request_ConsoleCommand, (ushort)CmdId.Request_ConsoleCommand, new Eleon.Modding.PString(command));
+            }
+        }
+
+        private void ConsoleCommand(String command)
+        {
+            GameAPI.Game_Request(CmdId.Request_ConsoleCommand, (ushort)CmdId.Request_ConsoleCommand, new Eleon.Modding.PString(command));
+        }
+
+        private void LogFile(String FileName, String FileData)
+        {
+            if (!System.IO.File.Exists("Content\\Mods\\AdminCommands\\" + FileName))
+            {
+                System.IO.File.Create("Content\\Mods\\AdminCommands\\" + FileName);
+            }
+            string FileData2 = FileData + Environment.NewLine;
+            System.IO.File.AppendAllText("Content\\Mods\\AdminCommands\\" + FileName, FileData2);
+        }
+
+        private List<string> NameFragment(string ChatMessage)
+        {
+            //GameAPI.Game_Request(CmdId.Request_ConsoleCommand, (ushort)CmdId.Request_ConsoleCommand, new Eleon.Modding.PString("SAY '-" + ChatMessage + "-'"));
+
+            //List<string> EmpyrionID = new List<string> { };
+            List<string> ExactMatches = new List<string> { };
+            List<string> NearMatches = new List<string> { };
+            if (ChatMessage.Contains(" "))
+            {
+                var Message = ChatMessage.Split(' ');
+                string message1 = Message[1];
+                foreach (string SteamID in PlayerDictionary.Keys)
+                {
+                    if (Convert.ToString(PlayerDictionary[SteamID].EmpyrionID) == Message[1])
+                    {
+                        ExactMatches.Add(SteamID);
+                    }
+                    else if (PlayerDictionary[SteamID].PlayerName == Message[1])
+                    {
+                        ExactMatches.Add(SteamID);
+                    }
+                    else if (PlayerDictionary[SteamID].PlayerName.Contains(Message[1]))
+                    {
+                        NearMatches.Add(SteamID);
+                    }
+                    else if (PlayerDictionary[SteamID].PlayerName.ToLower().Contains(Message[1].ToLower()))
+                    {
+                        NearMatches.Add(SteamID);
+                    }
+
+                }
+                //GameAPI.Game_Request(CmdId.Request_ConsoleCommand, (ushort)CmdId.Request_ConsoleCommand, new Eleon.Modding.PString("SAY 'ExactMatches.Count:" + ExactMatches.Count + "'"));
+                //GameAPI.Game_Request(CmdId.Request_ConsoleCommand, (ushort)CmdId.Request_ConsoleCommand, new Eleon.Modding.PString("SAY 'NearMatches.Count:" + NearMatches.Count + "'"));
+                if (ExactMatches.Count == 1)
+                {
+                    return ExactMatches;
+                }
+                else //if (ExactMatches.Count > 0)
+                {
+                    foreach (string item in NearMatches)
+                    {
+                        ExactMatches.Add(item);
+                    }
+                    return ExactMatches;
+                }
+            }
+            else
+            {
+                return ExactMatches;
+            }
+        }
+
+        public ItemStack[] buildItemStack(string job)
+        {
+            string[] bagLines = System.IO.File.ReadAllLines(job);
+            int itemStackSize = bagLines.Count();
+            ItemStack[] itStack = new ItemStack[itemStackSize];
+            for (int i = 0; i < itemStackSize; ++i)
+            {
+                string[] bagLinesSplit = bagLines[i].Split(',');
+                itStack[i] = new ItemStack(Convert.ToInt32(bagLinesSplit[1]), Convert.ToInt32(bagLinesSplit[2]));
+                itStack[i].slotIdx = Convert.ToByte(bagLinesSplit[0]);
+                itStack[i].ammo = Convert.ToInt32(bagLinesSplit[3]);
+                itStack[i].decay = Convert.ToInt32(bagLinesSplit[4]);
+            }
+            return itStack;
+        }
+
+        public struct Players
+        {
+            public string SteamID;
+            public int Admin;
+            public int EmpyrionID;
+            public int FactionID;
+            public string PlayerName;
+            public string Playfield;
+            public float x;
+            public float y;
+            public float z;
+            public int ClientID;
+        }
+        public class PlayerData
+        {
+            public static Players playerData(string steamID, int admin, int empyrionID, int factionID, string playername, string playfield, float coordX, float coordY, float coordZ, int clientID)
+            {
+                Players NewPlayer = new Players();
+                NewPlayer.SteamID = steamID;
+                NewPlayer.Admin = admin;
+                NewPlayer.EmpyrionID = empyrionID;
+                NewPlayer.FactionID = factionID;
+                NewPlayer.PlayerName = playername;
+                NewPlayer.Playfield = playfield;
+                NewPlayer.x = coordX;
+                NewPlayer.y = coordY;
+                NewPlayer.z = coordZ;
+                NewPlayer.ClientID = clientID;
+                return NewPlayer;
+            }
+        }
+
+        public ushort GetNewSeqNr()
+        {
+            if (SeqNrCounter == 35000)
+            {
+                SeqNrCounter = 1500;
+            }
+            else
+            {
+                SeqNrCounter = SeqNrCounter++;
+            }
+            return SeqNrCounter;
+        }
+
+
+        public void Game_Start(ModGameAPI gameAPI)
+        {
+            GameAPI = gameAPI;
+        }
+        public void Game_Event(CmdId cmdId, ushort seqNr, object data)
+        {
+            try
+            {
+                switch (cmdId)
+                {
+                    case CmdId.Event_ChatMessage: //Automatic
+                        break;
+                    case CmdId.Event_Faction_Changed://Automatic
+                        break;
+                    case CmdId.Event_GameEvent://Automatic
+                        break;
+                    case CmdId.Event_PdaStateChange://Automatic
+                        break;
+                    case CmdId.Event_Player_ChangedPlayfield://Automatic
+                        break;
+                    case CmdId.Event_Player_Connected://Automatic
+                        Id PlayerConnected = (Id)data;
+                        ushort NewSeqNr = GetNewSeqNr();
+                        GameAPI.Game_Request(CmdId.Request_Player_Info, NewSeqNr, new Id(PlayerConnected.id));
+                        SeqNrDict[NewSeqNr] = Request.SeqNrTracker(CmdId.Request_Player_Info, NewSeqNr, PlayerConnected.id);
+                        break;
+                    case CmdId.Event_Player_Disconnected://Automatic
+                        break;
+                    case CmdId.Event_Player_DisconnectedWaiting://Automatic
+                        break;
+                    case CmdId.Event_Playfield_Loaded://Automatic
+                        break;
+                    case CmdId.Event_Playfield_Unloaded://Automatic
+                        break;
+                    case CmdId.Event_Statistics://Automatic
+                        break;
+                    case CmdId.Event_TraderNPCItemSold://Automatic
+                        break;
+
+                }
+            }
+
+            catch (Exception ex)
+            {
+                LogFile("ERROR.txt", "Exception: " + ex.Message);
+                LogFile("ERROR.txt", "Exception: " + ex.Data);
+                LogFile("ERROR.txt", "Exception: " + ex.HelpLink);
+                LogFile("ERROR.txt", "Exception: " + ex.InnerException);
+                LogFile("ERROR.txt", "Exception: " + ex.Source);
+                LogFile("ERROR.txt", "Exception: " + ex.StackTrace);
+                LogFile("ERROR.txt", "Exception: " + ex.TargetSite);
+            }
+            if (SeqNrDict.ContainsKey(seqNr))
+            {
+                if (SeqNrDict[seqNr].requestID == cmdId)
+                    try
+                    {
+                        switch (cmdId)
+                        {
+
+                            case CmdId.Event_AlliancesAll: //Triggered
+                                break;
+                            case CmdId.Event_AlliancesFaction://Triggered
+                                break;
+                            case CmdId.Event_BannedPlayers://Triggered
+                                break;
+                            case CmdId.Event_ConsoleCommand://Triggered
+                                break;
+                            case CmdId.Event_Dedi_Stats://Triggered
+                                break;
+                            case CmdId.Event_Entity_PosAndRot://Triggered
+                                break;
+                            case CmdId.Event_Get_Factions://Triggered
+                                break;
+                            case CmdId.Event_GlobalStructure_List://Triggered
+                                break;
+                            case CmdId.Event_NewEntityId://Triggered
+                                break;
+                            case CmdId.Event_Player_Credits://Triggered
+                                break;
+                            case CmdId.Event_Player_GetAndRemoveInventory://Triggered
+                                break;
+                            case CmdId.Event_Player_Info://Triggered
+                                PlayerInfo PlayerInfoReceived = (PlayerInfo)data;
+                                PlayerDictionary[PlayerInfoReceived.playerName] = PlayerData.playerData(PlayerInfoReceived.steamId, PlayerInfoReceived.permission, PlayerInfoReceived.entityId, PlayerInfoReceived.factionId, PlayerInfoReceived.playerName, PlayerInfoReceived.playfield, Convert.ToInt32(PlayerInfoReceived.pos.x), Convert.ToInt32(PlayerInfoReceived.pos.y), Convert.ToInt32(PlayerInfoReceived.pos.z), PlayerInfoReceived.clientId);
+                                if (SeqNrDict[seqNr].empyrionID == PlayerInfoReceived.entityId)
+                                {
+
+                                }
+                                break;
+                            case CmdId.Event_Player_Inventory://Triggered
+                                break;
+                            case CmdId.Event_Player_ItemExchange://Triggered
+                                break;
+                            case CmdId.Event_Player_List://Triggered
+                                break;
+                            case CmdId.Event_Playfield_Entity_List://Triggered
+                                break;
+                            case CmdId.Event_Playfield_List://Triggered
+                                break;
+                            case CmdId.Event_Playfield_Stats://Triggered
+                                break;
+                            case CmdId.Event_Structure_BlockStatistics://Triggered
+                                break;
+                            case CmdId.Event_Ok://Other?
+                                break;
+                            case CmdId.Event_Error://Other?
+                                ErrorInfo err = (ErrorInfo)data;
+                                ErrorType err2 = (ErrorType)data;
+                                LogFile("ERROR.txt", "Event_ERROR: " + Convert.ToString(err2) + ": " + Convert.ToString(err));
+
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+
+                    catch (Exception ex)
+                    {
+                        LogFile("ERROR.txt", "Exception: " + ex.Message);
+                        LogFile("ERROR.txt", "Exception: " + ex.Data);
+                        LogFile("ERROR.txt", "Exception: " + ex.HelpLink);
+                        LogFile("ERROR.txt", "Exception: " + ex.InnerException);
+                        LogFile("ERROR.txt", "Exception: " + ex.Source);
+                        LogFile("ERROR.txt", "Exception: " + ex.StackTrace);
+                        LogFile("ERROR.txt", "Exception: " + ex.TargetSite);
+                    }
+            }
+        } 
+        public void Game_Update()
+        {
+        }
+        public void Game_Exit()
+        {
+        }
+    }
+}
---
+++ RequestTracker/RequestTracker.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Eleon.Modding;
+
+namespace Tracker
+{
+
+    public class Request
+    {
+        public ushort SeqNrCounter = 1500;
+        public Dictionary<Int32, Request> TrackRequest = new Dictionary<Int32, Request> { };
+
+        public struct TrackData
+        {
+            //public Int32 trackingID;
+            public ushort seqnr;
+            public Int32 empyrionID;
+            public Eleon.Modding.CmdId requestID;
+        }
+        public static TrackData SeqNrTracker(Eleon.Modding.CmdId RequestID, ushort seqNr, Int32 EmpyrionID)
+        {
+            TrackData NewData = new TrackData();
+            //NewData.trackingID = TrackingID;
+            NewData.seqnr = seqNr;
+            NewData.empyrionID = EmpyrionID;
+            NewData.requestID = RequestID;
+            return NewData;
+        }
+    }
+}
